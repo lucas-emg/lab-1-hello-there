cddb6d9cfd164ba3c6471691e921bf13b8fe165a
Lucas Mendes <lucas.gabriel3.lg@gmail.com>
2021-12-24 12:16:17 -0300

    Yout first Python Lab

---
+++ hello_there.py
@@ -0,0 +1,30 @@
+#This code should gather the user name, their birth year and calculate their age.
+#Then, tell them how many years are left for them to turn 100 years old.
+#It also should ask them where they are from (city and country) and give them a messagem about it.
+
+#Iteration 1 - 
+# 1 Ask user their name 
+# 2 Store their name in a variable
+# 3 Give them a message saying hello after that, like "Hey there, John! Glad to meet you!" (use string concatenation for it)
+# Your code goes below this line
+
+#Interation 2 -
+# 1 Ask the user what year their were born (from now one, you should always refer to them by their name)
+# 2 Store their age in a variable
+# 3 Create a variable that calculates how many years are left for them to become 100 year old.
+# 4 Give them a message about their age and how many years are left for them to become 100 years old.
+# Your code goes below this line
+
+#Interation 3 -
+# 1 Ask the user which city they are from
+# 2 Store the city in a variable
+# 3 Ask the user which country is this city in
+# 4 Store the country in a variable
+# 5 Give the user a message about how you would love to visit their city and country one day
+# Your code goes below this line
+
+#Interation 4 - 
+# 1 Ask the user how do they call Santa Claus in their native language
+# 2 Store it in a variable
+# 3 Give them a final message saying you wish them a Merry Xmas and that Santa Claus (use the one in their native language) brings them a lot of presents
+# Your code goes below this line
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Instructions
+
+You will right a Python script that asks the user their name, their birth year, calculates their age, asks them their hometown and country, and give them a Merry Xmas message using their native way to call Santa Claus
+
+You will use:
+
+- Basic math operations
+- input()
+- str() and int() to change the data type
+- String Concatenation
+- print()
+- variable declaration
+
+- 1: Fork this repository to your GitHub
+- 2: Clone this repository locally using git clone
+- 3: Work on the code
+- 4: Once finished, don't forget to git add and git commit the project
+- 5: Push the repository to GitHub
+- 6: Create a Pull Request to me once the project is finished!
+
+And of course, ask me if you need any help! =)
